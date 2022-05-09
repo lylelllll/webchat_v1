d699ec2f16e9de29ec8a4abd1b9b80027ca15dfa
shang liang <1179454895@qq.com>
2022-05-09 20:44:15 +0800

    query friend requests

--- friend/views.py
+++ friend/views.py
@@ -3,7 +3,23 @@ import json
 
 from account.models import Account
 from friend.models import FriendRequest
+from django.shortcuts import render, redirect
 
+
+def friend_requests(request, *args, **kwargs):
+	context = {}
+	user = request.user
+	if user.is_authenticated:
+		user_id = kwargs.get("user_id")
+		account = Account.objects.get(pk=user_id)
+		if account == user:
+			friend_requests = FriendRequest.objects.filter(receiver=account, is_active=True)
+			context['friend_requests'] = friend_requests
+		else:
+			return HttpResponse("You can't view another users friend requets.")
+	else:
+		redirect("login")
+	return render(request, "friend/friend_requests.html", context)
 
 def send_friend_request(request, *args, **kwargs):
     user = request.user
