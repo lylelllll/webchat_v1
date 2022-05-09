c26804db65026744401836c18756916da1270634
shang liang <1179454895@qq.com>
2022-05-09 16:53:07 +0800

    search Users

--- web_chat/urls.py
+++ web_chat/urls.py
@@ -28,6 +28,7 @@ from account.views import (
     register_view,
     login_view,
     logout_view,
+    account_search_view,
 )
 
 urlpatterns = [
@@ -37,6 +38,8 @@ urlpatterns = [
     path('login/', login_view, name="login"),
     path('logout/', logout_view, name="logout"),
     path('register/', register_view, name="register"),
+
+    path('search/', account_search_view, name="search"),
 
     # Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
     path('password_change/done/',
